609c273dbb88d3453689f4177882fded92ac8de7
lv910929 <lv910929@live.com>
2016-10-21 09:37:25 +0800

    完成MainActivity中fragment的切换

--- app/src/main/java/com/lv/fulipu/ui/activity/MainActivity.java
+++ app/src/main/java/com/lv/fulipu/ui/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -14,6 +15,8 @@ import android.widget.RelativeLayout;
 
 import com.lv.fulipu.R;
 import com.lv.fulipu.ui.base.BaseActivity;
+import com.lv.fulipu.ui.fragment.CollectFragment;
+import com.lv.fulipu.ui.fragment.MeiziFragment;
 import com.lv.fulipu.utils.ActivityUtil;
 import com.lv.fulipu.utils.DoubleClickUtil;
 import com.lv.fulipu.utils.IntentUtils;
@@ -35,6 +38,9 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     @BindView(R.id.frame_main)
     FrameLayout frameMain;
 
+    private MeiziFragment meiziFragment;
+    private CollectFragment collectFragment;
+
     private int navigationCheckedItemId = R.id.nav_fuli;
     private String navigationCheckedTitle = "福利";
     private static final String savedInstanceStateItemId = "navigationCheckedItemId";
@@ -46,17 +52,93 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         initUI();
+        initOtherDatas(savedInstanceState);
+        setDefaultFragment();
     }
 
     @Override
     protected void initUI() {
         super.initUI();
-        toolbarMain.setTitle(getString(R.string.app_name));
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawerLayout, toolbarMain, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.setDrawerListener(toggle);
         toggle.syncState();
         navView.setNavigationItemSelectedListener(this);
+    }
+
+    private void initOtherDatas(Bundle savedInstanceState) {
+        if (savedInstanceState != null && savedInstanceState.getInt(savedInstanceStateItemId) != 0) {
+            navigationCheckedItemId = savedInstanceState.getInt(savedInstanceStateItemId);
+            navigationCheckedTitle = savedInstanceState.getString(savedInstanceStateTitle);
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        setTitle(item.getTitle()); // 改变页面标题，标明导航状态
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.nav_fuli:
+            case R.id.nav_like:
+                navigationCheckedItemId = id;
+                navigationCheckedTitle = item.getTitle().toString();
+                setMenuSelection(id);
+                item.setChecked(true); // 改变item选中状态
+                break;
+            case R.id.nav_share:
+                IntentUtils.startAppShareText(MainActivity.this, "福利铺", getString(R.string.download_url));
+                break;
+            case R.id.nav_info:
+                ActivityUtil.startActivity(MainActivity.this, AboutActivity.class, null);
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    /**
+     * 设置默认的Fragment显示：如果savedInstanceState不是空，证明activity被后台销毁重建了，之前有fragment，就不再创建了
+     */
+    private void setDefaultFragment() {
+        setMenuSelection(navigationCheckedItemId);
+    }
+
+    private void setMenuSelection(int flag) {
+        toolbarMain.setTitle(navigationCheckedTitle);
+        // 开启一个Fragment事务
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        // 先隐藏掉所有的Fragment，以防止有多个Fragment显示在界面上的情况
+        hideFragments(fragmentTransaction);
+        switch (flag) {
+            case R.id.nav_fuli:
+                if (meiziFragment == null) {
+                    meiziFragment = new MeiziFragment();
+                    fragmentTransaction.add(R.id.frame_main, meiziFragment);
+                } else {
+                    fragmentTransaction.show(meiziFragment);
+                }
+                break;
+            case R.id.nav_like:
+                if (collectFragment == null) {
+                    collectFragment = new CollectFragment();
+                    fragmentTransaction.add(R.id.frame_main, collectFragment);
+                } else {
+                    fragmentTransaction.show(collectFragment);
+                }
+                break;
+
+        }
+        fragmentTransaction.commit();
+    }
+
+    private void hideFragments(FragmentTransaction transaction) {
+        if (meiziFragment != null) {
+            transaction.hide(meiziFragment);
+        }
+        if (collectFragment != null) {
+            transaction.hide(collectFragment);
+        }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
@@ -72,25 +154,4 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
             }
         }
     }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-        if (id == R.id.nav_fuli) {
-
-            drawerLayout.closeDrawer(GravityCompat.START);
-        } else if (id == R.id.nav_like) {
-
-            drawerLayout.closeDrawer(GravityCompat.START);
-        } else if (id == R.id.nav_share) {//分享
-            IntentUtils.startAppShareText(MainActivity.this, "福利铺", getString(R.string.download_url));
-            drawerLayout.closeDrawer(GravityCompat.START);
-        } else if (id == R.id.nav_info) {//关于
-            ActivityUtil.startActivity(MainActivity.this, AboutActivity.class, null);
-        }
-        return true;
-    }
-
 }
---
+++ app/src/main/java/com/lv/fulipu/ui/fragment/CollectFragment.java
@@ -0,0 +1,26 @@
+package com.lv.fulipu.ui.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lv.fulipu.R;
+import com.lv.fulipu.ui.base.BaseFragment;
+
+
+public class CollectFragment extends BaseFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_collect, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
